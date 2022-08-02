33e4a033e090fe904334adf70bb2e8d6c14ee799
Giossaurus <gihhskt@gmail.com>
2022-08-02 20:01:23 -0300

    geodb api fetch

--- react-weather-app/src/components/search/Search.js
+++ react-weather-app/src/components/search/Search.js
@@ -1,14 +1,21 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from '../../Api';
 
 const Search = ({onSearchChange}) => {
     
     const [search, setSearch] = useState(null);
 
+    const loadOptions = (inputValue) => {
+        return fetch('/cities', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+    }
+
     const handleOnChange = (searchData) => {
         setSearch(searchData);
         onSearchChange(searchData);
-
     }
 
     return(
@@ -17,9 +24,9 @@ const Search = ({onSearchChange}) => {
             debounceTimeout={600}
             value={search}
             onChange={handleOnChange}
+            loadOptions={loadOptions}
         />
     )
-    
 }
 
 export default Search;
